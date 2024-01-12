92074e2dfad6e1cf5b40cc11f721c4801ed8ef12
Thomas Lim <43063390+thomaslky@users.noreply.github.com>
2024-01-12 16:10:27 +0800

    Updated page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { MortgageApplication } from './ui-components';
 import { Authenticator } from '@aws-amplify/ui-react';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 
-function App() {
+function App({ signOut, user }) {
   return (    
      <Authenticator.Provider>
+      <h1>Hello { user.username }!</h1>
       <MortgageApplication />
-     </Authenticator.Provider>
+     </Authenticator.Provider> 
   );
 }
 
